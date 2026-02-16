3bb07fdbf2155afba438e146aa3afeb11412dfa1
toooooby64 <jalvarez5667@gmail.com>
2026-02-15 22:46:39 -0800

    Inital commit: Setting up site with PaperCSS

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<div class="paper container">
+<nav class="border fixed split-nav">
+  <div class="nav-brand">
+  </div>
+  <div class="collapsible">
+    <input id="collapsible1" type="checkbox" name="collapsible1">
+    <label for="collapsible1">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </label>
+    <div class="collapsible-body">
+      <ul class="inline">
+        <li><a href="/blog">Documentation</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="https://www.github.com/toooooby64">Github</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="card" style="width: 20rem;">
+  <img src="https://picsum.photos/768" alt="Card example image">
+
+  <div class="card-body">
+    <h4 class="card-title">My awesome Paper card!</h4>
+    <h5 class="card-subtitle">Nice looking subtitle.</h5>
+    <p class="card-text">Notice that the card width in this example have been set to 20rem, otherwise it will try to fill the current container/row where the card is.</p>
+    <a href="/blog" class="paper-btn">Let me go here!</a>
+  </div>
+</div>
+</div>
