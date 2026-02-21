ea80ee12229cc5c14c09e0e836fc330cca5cc9ff
toooooby64 <jalvarez5667@gmail.com>
2026-02-21 10:37:42 -0800

    Fixed read my blog button

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,3 +1,7 @@
+<script>
+	let showModal = $state(false);
+</script>
+
 <section class="row flex-center flex-middle section">
 	<div class="sm-12 md-6 lg-6 col herodiv">
 		<div>
@@ -9,7 +13,7 @@
 			</p>
 		</div>
 		<div style="display: flex; flex-direction: row; gap: 1rem; align-items: center;">
-			<a href="/blog" class="paper-btn btn-primary">Read my blog</a>
+			<label class="paper-btn btn-primary" for="blog-modal">Read my blog</label>
 			<a href="#contact" style="opacity: 0.8;">Contact me →</a>
 		</div>
 	</div>
@@ -18,6 +22,25 @@
 		<img src="/myself1.png" alt="Jason Alvarez" class="profile-img" />
 	</div>
 </section>
+<input class="modal-state" id="blog-modal" type="checkbox" bind:checked={showModal} />
+<div class="modal">
+	<label class="modal-bg" for="blog-modal"></label>
+	<div class="modal-body" style="width: 30%;">
+		<label class="btn-close" for="blog-modal">X</label>
+		<h4 class="modal-title">Blog is coming soon!</h4>
+		<h5 class="modal-subtitle">For now, enjoy this cool song I found</h5>
+		<div style="position: relative; padding-bottom: 56.25%; height: 0; margin-top: 1rem;">
+			<iframe
+				style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+				src={showModal ? 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1' : ''}
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+			></iframe>
+		</div>
+	</div>
+</div>
 
 <style>
 	.profile-img {
